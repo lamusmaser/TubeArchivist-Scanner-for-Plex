bc1e2bc08cea6339dff87292bd8defc13e11e940
lamusmaser <lamusmaser@gmail.com>
2023-06-19 21:19:02 -0600

    Adjust logging output.

--- Scanners/Series/Test Scanner.py
+++ Scanners/Series/Test Scanner.py
@@ -87,8 +87,9 @@ def setup():
     reload(sys)
     sys.setdefaultencoding("utf-8")
   global Log
-  Log = logging.getLogger('main')
+  Log = logging.getLogger(SOURCE)
   Log.setLevel(logging.DEBUG)
+  Log.basicConfig(format='%(asctime)s [%(name)s] %(levelname)s - %(message)s')
   set_logging()
 
   ### Populate PLEX_LIBRARY #############################################################################
@@ -102,6 +103,7 @@ def setup():
         Log.info(u'id: {:>2}, type: {:<6}, agent: {:<30}, scanner: {:<30}, library: {:<24}, path: {}'.format(directory.get("key"), directory.get('type'), directory.get("agent"), directory.get("scanner"), directory.get('title'), location.get("path")))
   except Exception as e:  Log.error("Exception: '%s', library_xml could not be loaded. X-Plex-Token file created?" % (e))
   Log.info(u"".ljust(157, '='))
+
 
 ### Read in a url #######################################################################################
 def read_url(url, data=None):
@@ -273,18 +275,11 @@ def Scan(path, files, mediaList, subdirs):
                 episode = video_metadata["episode"]
               except Exception as e: Log.error("Issue with setting metadata from Video using this metadata: '%s', Exception: '%s'" % (str(video_metadata), e))
 
-            write_to_test_output("Requesting helper with 'Media.Episode'.")
-            output_help_to_file("Media.Episode")
             tv_show = Media.Episode(str(show).encode("UTF-8"), str(season).encode("UTF-8"), None, str(title).encode("UTF-8"), str(season).encode("UTF-8"))
-            write_to_test_output("Requesting helper with 'tv_show.released_at'.")
-            output_help_to_file(tv_show.released_at)
+
             tv_show.released_at = str("{}-{}-{}".format(str(episode)[:3],str(episode)[4:5],str(episode)[6:7])).encode("UTF-8")
             tv_show.parts.append(i)
             Log.info("Adding episode to TV show list.")
-            write_to_test_output("Requesting helper with 'Media'.")
-            output_help_to_file(Media)
-            write_to_test_output("Requesting helper with 'mediaList'.")
-            output_help_to_file(mediaList)
             mediaList.append(tv_show)
             break
 
